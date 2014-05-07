4a83ae6ff33a402fa6c48245e90ef6eb0fd9a8eb
tsutaya <suzukitatsuya@net76-dhcp155.sfc.keio.ac.jp>
2014-05-07 13:12:14 +0900

    第四回課題

--- js/annimate.js
+++ js/annimate.js
@@ -3,23 +3,25 @@ var endFlipFoxkeh = function(){
 	foxkeh.removeEventListener("animationend", endFlipFoxkeh);
 
 	foxkeh.setAttribute("class", "");
-};
+};//documentでslidein-foxkeh imgを指定し動作が終了される。
 
 var endSlideinFoxkehAndStartFlipFoxkeh = function(){
 	var foxkeh = document.querySelector("#slidein-foxkeh img");
 
 	foxkeh.removeEventListener("animationend", endSlideinFoxkehAndStartFlipFoxkeh);
+	foxkeh.setAttribute("class", "flip");
 	foxkeh.addEventListener("animationend", endFlipFoxkeh);
 
-	foxkeh.setAttribute("class", "flip");
-};
+	
+};//documentでslidein-foxkeh imgを指定しスライドが終わるとフリップする。フリップの動作が終わるとendFlipFoxkeh に移る。
 
 var startSlideinFoxkeh = function(){
 	var foxkeh = document.querySelector("#slidein-foxkeh img");
 	foxkeh.setAttribute("class", "slidein");
 
 	foxkeh.addEventListener("animationend", endSlideinFoxkehAndStartFlipFoxkeh);
-};
+};//documentでslidein-foxkeh imgを指定しイメージがスライドする。スライドの動作が終わるとendSlideinFoxkehAndStartFlipFoxkehに移る。
 
 var slideinFoxkehButton = document.querySelector("#slidein-foxkeh button");
 slideinFoxkehButton.addEventListener("click", startSlideinFoxkeh);
+//documentでslide-foxkeh buttonを指定していてクリックされるとstartslideinFoxkehに移る。
